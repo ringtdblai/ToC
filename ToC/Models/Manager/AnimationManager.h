2945f0647efe131e8d0a32c2451c7e1913894db8
ringtdblai <ringtdblai@gmail.com>
2016-10-12 17:28:31 +0800

    Add ExportViewController and AnimationManager into project.

---
+++ ToC/Models/Manager/AnimationManager.h
@@ -0,0 +1,13 @@
+//
+//  AnimationManager.h
+//  ToC
+//
+//  Created by ringtdblai on 2016/10/12.
+//  Copyright © 2016年 Mobiusbobs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AnimationManager : NSObject
+
+@end
---
+++ ToC/Models/Manager/AnimationManager.m
@@ -0,0 +1,13 @@
+//
+//  AnimationManager.m
+//  ToC
+//
+//  Created by ringtdblai on 2016/10/12.
+//  Copyright © 2016年 Mobiusbobs. All rights reserved.
+//
+
+#import "AnimationManager.h"
+
+@implementation AnimationManager
+
+@end
